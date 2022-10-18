98837e372c9b01112ddc6c9dba5138a197aaf425
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-19 02:21:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, value) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === value.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, value) {
+    return drivers.filter(function (driver) { return driver.char[0] === value.char[0] })
+
+}
+
+function fuzzyMatch(drivers, value) {
+    return drivers.filter(function (driver) {
+        const value1 = driver.split('');
+        const value2 = value.split('');
+        return value1[0] === value2[0]
+    })
+}
+
+
+
+function matchName(drivers, value) {
+    return drivers.filter(({ name }) => name === value);
+
+
+}
